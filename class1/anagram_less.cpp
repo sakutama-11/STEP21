76ad6032f7717675292e9adfec8ee921a8fb3ad9
sakutama-11 <sakutama.swinzy8739@gmail.com>
2021-05-11 10:04:07 +0900

    Initial Commit

---
+++ class1/anagram_less.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i,n) for(int i=0; i<(n); i++)
+#define REP2(i,x,n) for(int i=x; i<(n); i++)
+#define ALL(n) begin(n),end(n)
+const long long INF = numeric_limits<long long>::max();
+//与えられた文字列を辞書ファイルから探して返す
+int main()
+{
+    return 0;
+}
