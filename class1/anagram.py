2c90308c9cd187417f65217b4eb1b3fc0da34f1d
sakutama-11 <sakutama.swinzy8739@gmail.com>
2021-05-13 13:44:40 +0900

    fix: separate searching and printing

--- class1/anagram.py
+++ class1/anagram.py
@@ -11,20 +11,22 @@ def sort_words(words):
     sorted_words.sort(key=lambda x: x[0])
     return sorted_words
 
-def print_anagram(word, words_list):
+def search_anagram(word, words_list):
     target = sorted_str(word)
     l = 0
     r = len(words_list)-1
+    found_words = []
     while(l < r):
         pivot = (l+r)//2
         if words_list[pivot][0] == target:
             found = words_list.pop(pivot)
-            print(found[1])
+            found_words.append(found[1])
             r = min(r, len(words_list))
         elif words_list[pivot][0] < target:
             l = pivot + 1
         else:
             r = pivot - 1
+    return found_words
 
 def main():
     f = open("words.txt", "r")
@@ -32,6 +34,8 @@ def main():
     f.close()
     sorted_words = sort_words(words)
     word = input("word: ")
-    print_anagram(word, sorted_words)
+    anagrams = search_anagram(word, sorted_words)
+    for anagram in anagrams:
+        print(anagram)
 if __name__ == "__main__":
     main()
