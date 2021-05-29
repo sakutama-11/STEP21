d3c60a698dad17a1acdd599e649d1ce0a27acad5
sakutama-11 <sakutama.swinzy8739@gmail.com>
2021-05-30 02:10:36 +0900

    class3 : 木構造×逆ポーランド記法を使ってc++でとく

--- class3/calculator.cpp
+++ class3/calculator.cpp
@@ -1,25 +1,36 @@
 #include <bits/stdc++.h>
 #include <string>
 #include <vector>
+#include <initializer_list>
+
 using namespace std;
 const long long INF = numeric_limits<long long>::max();
 
+enum type {
+    NUMBER,
+    OPEN,
+    CLOSE,
+    PLUS, 
+    MINUS,
+    TIMES,
+    DIVIDER
+};
 struct Token {
-    string type;
+    enum type type;
     double number;
 };
 
+
 struct Node {
-    int key;
     Token token;
     Node *parent, *left, *right;
 };
 
-void read_number(string &line, int &index, Token &token) {
+double read_number(string &line, int &index) {
     double number = 0;
     while (index < line.length() && line[index] >= '0' && line[index] <= '9') {
         int n = line[index] - '0';
-        number = number * 10 + (double)n;
+        number = number * 10 + (double) n;
         index++;
     }
     if (index < line.length() && line[index] == '.') {
@@ -27,70 +38,72 @@ void read_number(string &line, int &index, Token &token) {
         double decimal = 0.1;
         while (index < line.length() && line[index] >= '0' && line[index] <= '9') {
             int n = line[index] - '0';
-            number += n * decimal;
-            decimal /= 10;
+            number += (double) n * decimal;
+            decimal /= 10.0;
             index++;
         }
     }
-    token.type = "NUMBER";
-    token.number = number;
-    return; 
+    return number; 
+}
+
+void set_number(Token &token, double num) {
+    token.type = NUMBER;
+    token.number = num;
+    return;
 }
 
 void set_plus(string &line, int &index, Token &token) {
-    token.type = "PLUS";
+    token.type = PLUS;
     token.number = 0;
     index++;
     return;
 }
-
 
 void set_minus(string &line, int &index, Token &token) {
-    token.type = "MINUS";
+    token.type = MINUS;
     token.number = 0;
     index++;
     return;
 }
-
 
 void set_times(string &line, int &index, Token &token) {
-    token.type = "TIMES";
+    token.type = TIMES;
     token.number = 0;
     index++;
     return;
 }
-
 
 void set_divider(string &line, int &index, Token &token) {
-    token.type = "DIVIDER";
+    token.type = DIVIDER;
     token.number = 0;
     index++;
     return;
 }
-
 
 void set_open(string &line, int &index, Token &token) {
-    token.type = "OPEN";
+    token.type = OPEN;
     token.number = 0;
     index++;
     return;
 }
-
 
 void set_close(string &line, int &index, Token &token) {
-    token.type = "CLOSE";
+    token.type = CLOSE;
     token.number = 0;
     index++;
     return;
 }
+
 
 vector<Token> tokenize(string line) {
     vector<Token> tokens;
     int index = 0;
     while (index < line.length()) {
         Token token;
-        if (line[index] >= '0' && line[index] <= '9')
-            read_number(line, index, token);
+        if (line[index] >= '0' && line[index] <= '9'){
+            double number = read_number(line, index);
+            set_number(token, number);
+        }
         else if (line[index] == '+') set_plus(line, index, token);
         else if (line[index] == '-') set_minus(line, index, token);
         else if (line[index] == '*') set_times(line, index, token);
@@ -106,44 +119,160 @@ vector<Token> tokenize(string line) {
     return tokens;
 }
 
-Node make_tree(vector<Token> tokens, int &index) {
-    Node n;
-    Node *root;
-    Node *np;
-    while (index <= tokens.size()) {
+
+void add_tree_left(Node* &root, Node* &np, Node* node) {
+    root = node;
+    np = node;
+}
+
+void add_node_left(Node* &root, Node* &np, Node* node, Token token) {
+    node->token = token;
+    root = node;
+}
+
+void add_tree_right(Node* &root, Node* &np, Node* node) {
+    node->parent = np;
+    np->right = node;
+}
+
+
+void add_node_right(Node* &root, Node* &np, Node* node, Token token) {
+    node->token = token;
+    node->parent = np;
+    np->right = node;
+}
+
+void add_operator_top(Node* &root, Node* &np, Node* node, Token token) {
+    node->token = token;
+    node->left = root;
+    root->parent = node;
+    np = node;
+    root = node;
+}
+
+void add_operator_right(Node* &root, Node* &np, Node* node, Token token) {
+    node->token = token;
+    node->parent = np;
+    node->left = np->right;
+    np->right = node;
+    np = node;
+}
+
+Node* make_tree(vector<Token> &tokens, int &index, Node* start) {
+    Node *root = start;
+    Node *np = start;
+    while (index < tokens.size()) {
         Token token = tokens.at(index);
+        Node *new_node = new Node;
         switch (token.type) {
-            case "OPEN":
+            case NUMBER:
+                if (np == start) add_node_left(root, np, new_node, token);
+                else add_node_right(root, np, new_node, token);
                 index++;
-                make_tree(tokens, index);
-                // numberと同じ処理
                 break;
-            case "CLOSE":
+            case OPEN:
+                index++;
+                if (np == start) add_tree_left(root, np, make_tree(tokens, index, start));
+                else add_tree_right(root, np, make_tree(tokens, index, start));
+                delete new_node;
+                break;
+            case CLOSE:
+                delete new_node;
                 index++;
                 return root;
-            case "PLUS":
-            case "MINUS":
-                add_plus_minus(root, np);
+            case PLUS:
+            case MINUS:
+                add_operator_top(root, np, new_node, token);
                 index++;
                 break;
-            case "TIMES":
-            case "DIVIDER":
-                add_times_divider(root, np);
-                index++;
-                break;
-            case "NUMBER":
-                add_number(root, np, tokens.at(index).number);
+            case TIMES:
+            case DIVIDER:
+                if (np == start) add_operator_top(root, np, new_node, token);
+                else add_operator_right(root, np, new_node, token);
                 index++;
                 break;
         }
     }
-    return *root;
+    return root;
+}
+
+double evaluate(Node *node, stack<Token> &stack) {
+    cout << "evaluate" << endl;
+    if (node == nullptr) {
+        cout << "this is null" << endl;
+        return 0;
+    }
+    cout << node <<endl;
+    evaluate(node->left, stack);
+    evaluate(node->right, stack);
+    Token token = node->token;
+    if (token.type == NUMBER) {
+        cout << token.number << endl;
+        stack.push(token);
+    } else {
+        cout << token.type << endl;
+        Token b_token = stack.top();
+        stack.pop();
+        Token a_token = stack.top();
+        stack.pop();
+        Token new_token = {NUMBER, 0};
+        double a = a_token.number;
+        double b = b_token.number;
+        switch (token.type) {
+            case PLUS:                    
+                new_token.number = a + b;
+                stack.push(new_token);
+                break;
+            case MINUS:
+                new_token.number = a - b;
+                stack.push(new_token);
+                break;
+            case TIMES:
+                new_token.number = a * b;
+                stack.push(new_token);
+                break;
+            case DIVIDER:
+                new_token.number = a / b;
+                stack.push(new_token);
+                break;
+            }
+    }
+    return stack.top().number;
+}
+
+void test(string line, double answer) {
+    vector<Token> tokens = tokenize(line);
+    Node start;
+    int index = 0;
+    Node *root = make_tree(tokens, index, &start);
+    cout << "tree was made"<< endl;
+    stack<Token> s;
+    double result = evaluate(root, s);
+    if (fabs(answer - result) < 0.000000001) {
+        cout << "PASS: ";
+    } else {
+        cout << "FAIL: " << line << "should be " << answer << " but, ";
+    }
+    cout << line << "=" << result << endl;
+    return;
 }
 
 int main()
 {
-    string line = "(3*4.2/2+5)-((14-3)/3-2)";
-    vector<Token> tokens = tokenize(line);
-    make_tree(tokens, 0);
+    test("1", 1);
+    test("3+2", 5);
+    test("3-2", 1);
+    test("3*2", 6);
+    test("3/2", 1.5);
+    test("2.5+3.14", 5.64);
+    test("1+2+3", 6);
+    test("1*2*3", 6);
+    test("1+2*3", 7);
+    test("(1)", 1);
+    test("((1))", 1);
+    test("(1+2)*3", 9);
+    test("3+2*(4+5)", 21);
+    test("3+2*(4-5/2)", 6);
+    test("(3*4.2/2+5)-((14-3)/3-2)", 9.633333333333);
     return 0;
 }
